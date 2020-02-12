a4f061460e3f112005f0aad447172f3b3d8193b9
Jason <jace.chan@mail.utoronto.ca>
2020-02-12 01:21:56 -0500

    [SHIB-33] get and add post should be done

--- routes/queries.js
+++ routes/queries.js
@@ -83,7 +83,42 @@ exports.getPages = function (request, response) {
 }
 
 exports.addPost = function (request, response) {
-	response.status(403).end()
+
+	//need to check that atleast a title and page id exist.
+	body('title').exists().withMessage("Missing Title Parameter").bail()
+	  .matches(/^[a-zA-Z0-9 ]+$/i).withMessage("Invalid Title Parameter").bail().escape(),
+	body('page_id').exists().withMessage("Missing Page Id Parameter").bail()
+	  .isInt().withMessage("Invalid Page Id Parameter").bail().escape(),
+
+
+	async function (req, res, next) {
+		//see if we have any errors
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			// if there are errors, we return 400
+			res.status(400).json({ errors: errors.array() });
+			return;
+		}
+		
+		//here may be have to check if post is already in db?
+
+		db.task(async t => { //try to add to the db
+			const result = await t.one(addPostQuery, [req.body.title]);
+			return result;
+		}).then (result => {
+
+			if ("title" in result) {
+				// return 200 if we successfully added the page
+				res.status(200).send(`Page inserted with title ${result.title} and page ${result.page_id}`); 
+			} else {
+				// return 400 if we unsuccessfully added the page
+				res.status(400).send("Unable to insert the page");
+			}
+		})
+		// We want to catch any exception else your program will crash :) have fun with that 
+		.catch(e => {res.status(500); res.send(sendError(500, '/api' + req.url + ' error ' + e))})
+	}
 }
 
 exports.getPost = function (request, response) {
