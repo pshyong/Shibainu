582183ecf2701b9ee9923cdfe1fff5c4f397def8
Daffy Chu <danielchu0710@hotmail.com>
2020-02-12 01:35:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 const pgp = require('pg-promise')();
 
 const db = pgp({
-			  host: process.env.DB_HOST,
-			  user: process.env.DB_USER,
-			  database: process.env.DB_NAME,
-			  password: process.env.DB_PASS,
-			  port: process.env.DB_PORT,
-			});
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
 
 module.exports = db;
