ddef7f0536f2d0651304b1a7fc3e84671d478f14
Daffy Chu <danielchu0710@hotmail.com>
2020-02-22 01:20:17 -0500

    [SHIB-63] Added a sample test case

--- __tests__/API.test.js
+++ __tests__/API.test.js
@@ -1,3 +1,12 @@
-it('Testing to see if Jest works', () => {
-    expect(1).toBe(1)
+const request = require('supertest');
+const app = require('../app');
+
+// ! Note: This is for testing purposes, to remove after merge and with correct api
+describe('A testing endpoint', () => {
+  it('Should get a page back', async () => {
+    const res = await request(app)
+    .get('/api/v1/pages/page')
+    .send()
+    expect(res.statusCode).toEqual(200)
   })
+})
