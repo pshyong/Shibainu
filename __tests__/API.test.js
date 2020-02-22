6e9e8537b01b66e91d3a5b46a4a307f8381f67bc
Daffy Chu <danielchu0710@hotmail.com>
2020-02-22 01:22:29 -0500

    [SHIB-63] Add links to doc

--- __tests__/API.test.js
+++ __tests__/API.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
 const app = require('../app');
 
+// ? Please take a look at
+// ? https://jestjs.io/docs/en/getting-started
+// ? https://github.com/visionmedia/supertest
+
+
 // ! Note: This is for testing purposes, to remove after merge and with correct api
 describe('A testing endpoint', () => {
   it('Should get a page back', async () => {
