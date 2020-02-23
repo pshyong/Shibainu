3d7c5fbac2b0d1529b524dce43950e6fbf82b088
Daffy Chu <danielchu0710@hotmail.com>
2020-02-23 13:17:33 -0500

    [SHIB-56] Update routing to include new thread creation template

--- routes/UI/subpage.js
+++ routes/UI/subpage.js
@@ -6,13 +6,20 @@ router.get('/', function(req, res, next) {
     res.render('pages/subpage', { title: 'shibainu | subpage', name: req.params.name });
 });
 
-router.route('/c/:cat_name/?')
+router.route('/c/:cat_name/')
     .get(function(req, res) {
       res.render('pages/category', { title: 'shibainu | subpage', name: req.params.name, category: req.params.cat_name, moment: moment});
+});
+
+router.route('/c/:cat_name/post-thread')
+    .get(function(req, res) {
+        res.render('pages/newthread', { title: 'shibainu | subpage', name: req.params.name, category: req.params.cat_name, moment: moment});
 });
 
 router.route('/c/:cat_name/threads/:thread_name/?')
     .get(function(req, res) {
         res.render('pages/threads', { title: 'shibainu | subpage', name: req.params.name, category: req.params.cat_name, moment: moment});
 });
+
+
 module.exports = router
