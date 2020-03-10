c8ed24cba44f65c09881e27dab510b64b1f338da
chanja60 <jace.chan@mail.utoronto.ca>
2020-03-10 14:46:45 -0400

    [SHIB-12] Got post thread to now display content to log

---
+++ public/assets/js/createThread_script.js
@@ -0,0 +1,8 @@
+function asdf(){
+    console.log(document.getElementById('subject').value)
+    console.log(document.getElementById('category').value)
+    console.log(document.getElementById('content').value)
+
+    console.log("fuk")
+
+}
