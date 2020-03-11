d519d8c1d7f9043ee37a2fbff48904083525b481
Jason <jace.chan@mail.utoronto.ca>
2020-03-11 03:44:33 -0400

    [SHIB-12] Threads can now be made, but currently they are all hard coded to go in subcat 1

--- public/assets/js/createThread_script.js
+++ public/assets/js/createThread_script.js
@@ -3,6 +3,13 @@ function asdf(){
     console.log(document.getElementById('category').value)
     console.log(document.getElementById('content').value)
 
-    console.log("fuk")
+    var sub_cat_id = 1;
+    var subject = document.getElementById('subject').value;
+    var content = document.getElementById('content').value;
+
+    console.log({sub_cat_id,subject,content })
+
+    	
+    $.post( "http://localhost:3000/api/v1/pages/thread", {sub_cat_id,subject,content } );
 
 }
