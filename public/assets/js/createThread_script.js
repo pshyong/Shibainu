a948a5a7b3a13d337d6c432ff15f22580abaf337
Jason <jace.chan@mail.utoronto.ca>
2020-03-12 03:21:30 -0400

    [SHIB-12] When making a new thread, catergories can now be selected

--- public/assets/js/createThread_script.js
+++ public/assets/js/createThread_script.js
@@ -1,36 +1,17 @@
 function asdf(page_name){
-    console.log(document.getElementById('subject').value)
-    console.log(document.getElementById('category').value)
-    console.log(document.getElementById('content').value)
 
-    var sub_cat_id = 1;
+
+    var sub_cat_id = document.getElementById('category').value;
     var subject = document.getElementById('subject').value;
     var content = document.getElementById('content').value;
 
-    console.log({sub_cat_id,subject,content })
-
-    	
+	
     $.post( "http://localhost:3000/api/v1/pages/thread", {sub_cat_id,subject,content } );
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "http://localhost:3000/api/v1/pages/Page/" + page_name, false ); // false for synchronous request
-    xmlHttp.send( null );
-    console.log(xmlHttp.responseText)
-    //return xmlHttp.responseText;
-
-    
 
 
 }
 
 function getCats(page_name){
-    /*
-    console.log(page_name)
-    var info = $.get( "http://localhost:3000/api/v1/pages/Page/Jason");
-    var info = $.getJSON( "http://localhost:3000/api/v1/pages/Page", page_name );
-    console.log(info)
-    */
-
 
    var xmlHttp = new XMLHttpRequest();
    xmlHttp.open( "GET", "http://localhost:3000/api/v1/pages/Page/Jason", false ); // false for synchronous request
@@ -47,13 +28,15 @@ function getCats(page_name){
        for(j = 0; j < res.sub_category[i].length ; j++){
             console.log(res.category[i].subject + "/" + res.sub_category[i][j].subject)
 
+            var x = document.getElementById("category");
+            var c = document.createElement("option");
+            c.text = res.category[i].subject + "/" + res.sub_category[i][j].subject;
+            c.value = res.sub_category[i][j].sub_cat_id
+            x.options.add(c, 1);
+
        }
 
    }
-
-
-
-
 
    //return xmlHttp.responseText;
 }
