1bb4b28dc1def13cae1f83a894d5c1541f8b1b5c
Jason <jace.chan@mail.utoronto.ca>
2020-03-13 15:55:57 -0400

    [SHIB-12] Put getCats function into a xhttp function

--- public/assets/js/createThread_script.js
+++ public/assets/js/createThread_script.js
@@ -13,16 +13,37 @@ function post_thread(page_name){
 
 function getCats(page_name){
 
-   var xmlHttp = new XMLHttpRequest();
-   xmlHttp.open( "GET", "http://localhost:3000/api/v1/pages/Page/" + page_name, false ); 
-   xmlHttp.send( null );
 
-   var res = JSON.parse(xmlHttp.responseText);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            result = JSON.parse(xhttp.responseText);
+            var i;
+            for(i =0; i < result.sub_category.length ; i++){
+                var j;
+                for(j = 0; j < result.sub_category[i].length ; j++){
+                    console.log(result.category[i].subject + "/" + result.sub_category[i][j].subject)
 
-   var i;
-   for(i =0; i < res.sub_category.length ; i++){
-       var j;
-       for(j = 0; j < res.sub_category[i].length ; j++){
+                    var x = document.getElementById("category");
+                    var c = document.createElement("option");
+                    c.text = result.category[i].subject + "/" + result.sub_category[i][j].subject;
+                    c.value = result.sub_category[i][j].sub_cat_id
+                    x.options.add(c, 1);
+
+                }
+
+            }
+            
+            
+        }
+    };
+
+
+/*
+    var i;
+    for(i =0; i < res.sub_category.length ; i++){
+        var j;
+        for(j = 0; j < res.sub_category[i].length ; j++){
             console.log(res.category[i].subject + "/" + res.sub_category[i][j].subject)
 
             var x = document.getElementById("category");
@@ -31,9 +52,12 @@ function getCats(page_name){
             c.value = res.sub_category[i][j].sub_cat_id
             x.options.add(c, 1);
 
-       }
+        }
 
-   }
+    }
+*/
+    xhttp.open( "GET", "http://localhost:3000/api/v1/pages/Page/" + page_name, false ); 
+    xhttp.send( null );
 
    //return xmlHttp.responseText;
 }
