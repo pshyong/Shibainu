251d4dd0aa0008d8f52cd4cbf1e255e124478051
Rach Ilagan <ilagan.rach@gmail.com>
2020-03-25 16:58:40 -0400

    Add user api for GET and DELETE requests

---
+++ routes/API/users.js
@@ -0,0 +1,76 @@
+const db = require('../../config');
+
+const { param, validationResult } = require('express-validator');
+
+function sendJSON(statusCode, payload) {
+  return JSON.stringify({ status_code: statusCode, payload: payload });
+}
+
+function sendError(statusCode, message, additionalInfo = {}) {
+  return JSON.stringify({
+    status_code: statusCode,
+    error: { message: message, additional_information: additionalInfo }
+  });
+}
+
+exports.getUser = [
+  param('user_account_id')
+    .exists()
+    .withMessage('Missing User Account ID Parameter')
+    .bail(),
+
+  async function(req, res, next) {
+    const errors = validationResult(req);
+    const userId = req.param.user_account_id;
+    if (errors) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    try {
+      const user = await db.oneOrNone(
+        'SELECT * FROM User_account WHERE user_account_id=$1;',
+        [userId]
+      );
+
+      if (user) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).send('User does not exist');
+      }
+    } catch (err) {
+      res.status(500).send(sendError(500, `/api${req.url}error${err}`));
+    }
+  }
+];
+
+exports.updateUser = [];
+
+exports.deteleUser = [
+  param('user_account_id')
+    .exists()
+    .withMessage('Missing User Account ID Parameter')
+    .bail(),
+
+  async function(req, res, next) {
+    const errors = validationResult(req);
+    const userId = req.param.user_account_id;
+    if (errors) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    try {
+      const user = await db.oneOrNone(
+        'DELETE FROM User_account WHERE user_account_id=$1;',
+        [userId]
+      );
+
+      if (user) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).send('User does not exist');
+      }
+    } catch (err) {
+      res.status(500).send(sendError(500, `/api${req.url}error${err}`));
+    }
+  }
+];
